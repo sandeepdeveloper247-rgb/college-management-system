e0ca15456865e9f8e98378cd5c0aaf46e479b934
SANDEEPPRADHAN193 <sandeep.pradhan1011@gmail.com>
2026-08-02 15:40:26 +0530

    Implement JWT-based user login

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User=require("../models/User");
 const bcrypt=require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function registerUser(req,res){
   try{
@@ -54,6 +55,62 @@ async function registerUser(req,res){
     });
   }
 }
+
+async function loginUser(req,res){
+  try{
+    const{email,password}=req.body;
+
+    if(!email || !password){
+      return res.status(400).json({
+        message: "Email and password are required."
+      });
+    }
+
+    const user=await User.findOne({email});
+
+    if(!user){
+      return res.status(404).json({
+        message: "User not found."
+      });
+    }
+
+    const isMatch=await bcrypt.compare(password,user.password);
+    if(!isMatch){
+      return res.status(401).json({
+        message: "Invalid credentials."
+      });
+    }
+    // console.log(process.env.JWT_SECRET);
+    const token=jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+      user:{
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+  }catch(error){
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error."
+    });
+  }
+}
+
 module.exports={
-  registerUser
+  registerUser,
+  loginUser
 };
